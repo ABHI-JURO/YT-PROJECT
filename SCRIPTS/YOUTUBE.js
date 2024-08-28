2e23a50473b8fab2be4fcb80f60ae0a1634a8f2c
Abhishek Shrivastava <138434193+ABHI-JURO@users.noreply.github.com>
2024-08-28 16:06:17 +0530

    made the project interactive with JS

---
+++ SCRIPTS/YOUTUBE.js
@@ -0,0 +1,48 @@
+document.querySelector("#thumbnail_1").addEventListener("click", function() {
+    window.open("https://www.youtube.com/watch?v=n2RNcPRtAiY","_blank");
+});
+
+document.querySelector("#thumbnail_2").addEventListener("click", function() {
+    window.open("https://www.youtube.com/watch?v=mP0RAo9SKZk","_blank");
+});
+
+document.querySelector("#thumbnail_3").addEventListener("click", function() {
+    window.open("https://www.youtube.com/watch?v=FgjPQQeTh1w","_blank");
+});
+
+document.querySelector("#thumbnail_4").addEventListener("click", function() {
+    window.open("https://www.youtube.com/watch?v=094y1Z2wpJg","_blank");
+});
+
+document.querySelector("#thumbnail_5").addEventListener("click", function() {
+    window.open("https://www.youtube.com/watch?v=86CQq3pKSUw","_blank");
+});
+
+document.querySelector("#thumbnail_6").addEventListener("click", function() {
+    window.open("https://www.youtube.com/watch?v=yXWw0_UfSFg","_blank");
+});
+
+document.querySelector("#thumbnail_7").addEventListener("click", function() {
+    window.open("https://www.youtube.com/watch?v=fNVa1qMbF9Y","_blank");
+});
+
+document.querySelector("#thumbnail_8").addEventListener("click", function() {
+    window.open("https://www.youtube.com/watch?v=lFm4EM1juls","_blank");
+});
+
+document.querySelector("#thumbnail_9").addEventListener("click", function() {
+    window.open("https://www.youtube.com/watch?v=ixmxOlcrlUc","_blank");
+});
+
+document.querySelector("#thumbnail_10").addEventListener("click", function() {
+    window.open("https://www.youtube.com/watch?v=R2vXbFp5C9o","_blank");
+});
+
+document.querySelector("#thumbnail_11").addEventListener("click", function() {
+    window.open("https://www.youtube.com/watch?v=0nZuYyXET3s","_blank");
+});
+
+document.querySelector("#thumbnail_12").addEventListener("click", function() {
+    window.open("https://www.youtube.com/watch?v=9iMGFqMmUFs","_blank");
+});
+
